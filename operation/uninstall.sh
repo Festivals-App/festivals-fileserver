e403a955a55590349ca0fce62b5d09c04e879d14
Simon Gaus <phisto05@gmail.com>
2021-08-12 11:59:54 +0200

    Update uninstall script

--- operation/uninstall.sh
+++ operation/uninstall.sh
@@ -4,7 +4,7 @@
 #
 # Removes the firewall configuration, uninstalls go, git and the festivals-fileserver and stops and removes it as a service.
 #
-# (c)2020 Simon Gaus
+# (c)2020-2021 Simon Gaus
 #
 
 # Move to working directory
@@ -25,21 +25,11 @@ echo "Removed systemd service"
 sleep 1
 
 # Remove the firewall configuration.
-# Supported firewalls: ufw and firewalld
 # This step is skipped under macOS.
 #
-if command -v firewalld > /dev/null; then
+if command -v ufw > /dev/null; then
 
-  firewall-cmd --permanent --remove-service=festivals-fileserver >/dev/null
-  rm -f /etc/firewalld/services/festivals-fileserver.xml >/dev/null
-  rm -f /etc/firewalld/services/festivals-fileserver.xml.old >/dev/null
-  firewall-cmd --reload >/dev/null
-  echo "Removed firewalld configuration"
-  sleep 1
-
-elif command -v ufw > /dev/null; then
-
-  ufw delete allow 1919/tcp >/dev/null
+  ufw delete allow 1910/tcp >/dev/null
   echo "Removed ufw configuration"
   sleep 1
 
@@ -50,9 +40,8 @@ fi
 
 # Remove go
 #
-rm -R /usr/local/go
-rm /usr/local/bin/go
-rm /usr/local/bin/gofmt
+apt-get --purge remove golang -y
+apt autoremove -y
 echo "Removed go"
 sleep 1
 
