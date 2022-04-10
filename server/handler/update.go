bf63583f581e33d1841a906ead9138bf909cb3c0
Simon Gaus <phisto05@gmail.com>
2022-04-10 23:19:02 +0200

    Change update request handling

--- server/handler/update.go
+++ server/handler/update.go
@@ -5,17 +5,10 @@ import (
 
 	"github.com/Festivals-App/festivals-fileserver/server/config"
 	"github.com/Festivals-App/festivals-gateway/server/update"
-	"github.com/rs/zerolog/log"
 )
 
 func MakeUpdate(conf *config.Config, w http.ResponseWriter, _ *http.Request) {
 
-	newVersion, err := update.RunUpdate("https://github.com/Festivals-App/festivals-fileserver/releases/latest", "/usr/local/festivals-fileserver/update.sh")
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to update")
-		respondError(w, http.StatusInternalServerError, "Failed to update")
-		return
-	}
-
-	respondString(w, http.StatusAccepted, newVersion)
+	respondCode(w, http.StatusAccepted)
+	go update.RunUpdate("https://github.com/Festivals-App/festivals-fileserver/releases/latest", "/usr/local/festivals-fileserver/update.sh")
 }
