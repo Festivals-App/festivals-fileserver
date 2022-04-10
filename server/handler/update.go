e6c60c2fe3edeabc311438106e0f5e2da7be9671
Simon Gaus <phisto05@gmail.com>
2022-04-11 01:20:15 +0200

    Fix update

--- server/handler/update.go
+++ server/handler/update.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"github.com/Festivals-App/festivals-fileserver/server/config"
+	"github.com/Festivals-App/festivals-fileserver/server/status"
 	"github.com/Festivals-App/festivals-gateway/server/update"
 	"github.com/rs/zerolog/log"
 )
 
 func MakeUpdate(conf *config.Config, w http.ResponseWriter, _ *http.Request) {
 
-	newVersion, err := update.RunUpdate("https://github.com/Festivals-App/festivals-fileserver/releases/latest", "/usr/local/festivals-fileserver/update.sh")
+	newVersion, err := update.RunUpdate(status.ServerVersion, "https://github.com/Festivals-App/festivals-fileserver/releases/latest", "/usr/local/festivals-fileserver/update.sh")
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to update")
 		respondError(w, http.StatusInternalServerError, "Failed to update")
