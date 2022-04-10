f85ee1a48f3169f4cbbd2809a717233d73a5664f
Simon Gaus <phisto05@gmail.com>
2022-04-10 23:50:21 +0200

    Fix wrong references

--- server/handler/update.go
+++ server/handler/update.go
@@ -10,7 +10,7 @@ import (
 
 func MakeUpdate(conf *config.Config, w http.ResponseWriter, _ *http.Request) {
 
-	newVersion, err := update.RunUpdate("https://github.com/Festivals-App/festivals-gateway/releases/latest", "/usr/local/festivals-gateway/update.sh")
+	newVersion, err := update.RunUpdate("https://github.com/Festivals-App/festivals-fileserver/releases/latest", "/usr/local/festivals-fileserver/update.sh")
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to update")
 		respondError(w, http.StatusInternalServerError, "Failed to update")
